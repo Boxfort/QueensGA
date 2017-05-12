d5e26e1f4df803102781d21eb5b0c48f8a757ad3
Jack Anderson <40208539@live.napier.ac.uk>
2017-05-12 22:06:56 +0100

    added tournament select

--- Solver.cs
+++ Solver.cs
@@ -13,11 +13,11 @@ namespace QueensGA
         private const int TOURNAMENT_SIZE = 5;
         private const bool ELITISM = true;
 
-        Population population;
+        private Population _population;
 
         public Solver()
         {
-            population = new Population(POPULATION_SIZE, true);
+            _population = new Population(POPULATION_SIZE, true);
         }
 
         public void EvolvePopulation()
@@ -27,20 +27,38 @@ namespace QueensGA
             int elitismOffset = 0;
             if (ELITISM)
             {
-                newPopulation.SaveSolution(0, population.GetFittest());
+                newPopulation.SaveSolution(0, _population.GetFittest());
                 elitismOffset = 1;
             }
 
             for (int i = elitismOffset; i < POPULATION_SIZE; i++)
             {
-               //Select parents
-               //Create child
-               //Add child to population
+                Solution parent1 = TournamentSelect(_population);
+                Solution parent2 = TournamentSelect(_population);
+                Solution child = new Solution(parent1, parent2);
+
+                newPopulation.SaveSolution(i, child);
             }
 
             //Mutate Population
+
+            _population = newPopulation;
         }
 
-        
+        private Solution TournamentSelect(Population population)
+        {
+            // Create a tournament population
+            Population tournament = new Population(TOURNAMENT_SIZE, false);
+            // For each place in the tournament get a random candidate tour and
+            // add it
+            for (int i = 0; i < TOURNAMENT_SIZE; i++)
+            {
+                Random rand = new Random();
+                tournament.SaveSolution(i, population.GetSolution(rand.Next(POPULATION_SIZE)));
+            }
+            // Get the fittest tour
+            Solution fittest = tournament.GetFittest();
+            return fittest;
+        }
     }
 }
