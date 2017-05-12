b648b43c231d3a9a5669ae8eb00189c7c3cb2d78
Jack Anderson <40208539@live.napier.ac.uk>
2017-05-12 22:00:16 +0100

    started evolution

--- Solver.cs
+++ Solver.cs
@@ -8,5 +8,39 @@ namespace QueensGA
 {
     class Solver
     {
+        private const int POPULATION_SIZE = 100;
+        private const double MUTATION_RATE = 0.015;
+        private const int TOURNAMENT_SIZE = 5;
+        private const bool ELITISM = true;
+
+        Population population;
+
+        public Solver()
+        {
+            population = new Population(POPULATION_SIZE, true);
+        }
+
+        public void EvolvePopulation()
+        {
+            Population newPopulation = new Population(POPULATION_SIZE, false);
+
+            int elitismOffset = 0;
+            if (ELITISM)
+            {
+                newPopulation.SaveSolution(0, population.GetFittest());
+                elitismOffset = 1;
+            }
+
+            for (int i = elitismOffset; i < POPULATION_SIZE; i++)
+            {
+               //Select parents
+               //Create child
+               //Add child to population
+            }
+
+            //Mutate Population
+        }
+
+        
     }
 }
