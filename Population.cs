f78dc76a03dffe8817d1877d25834f33d3bf44b7
Jack Anderson <40208539@live.napier.ac.uk>
2017-05-12 21:04:59 +0100

    GetFittest method

--- Population.cs
+++ Population.cs
@@ -8,6 +8,33 @@ namespace QueensGA
 {
     class Population
     {
+        Solution[] solutions;
 
+        public Population(int populationSize)
+        {
+            solutions = new Solution[populationSize];
+        }
+
+        /// <summary>
+        /// Evaluate the population and return the fittest solution.
+        /// </summary>
+        /// <returns>Solution with the lowest fitness.</returns>
+        public Solution GetFittest()
+        {
+            Solution fittest = solutions[0];
+            int fittestFitness = fittest.EvaluateFitness();
+
+            for(int i = 0; i < solutions.Length; i++)
+            {
+                int fitness = solutions[i].EvaluateFitness();
+                if(fitness > fittestFitness)
+                {
+                    fittest = solutions[i];
+                    fittestFitness = fitness;
+                }
+            }
+
+            return fittest;
+        }
     }
 }
